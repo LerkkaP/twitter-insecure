aeb34cebb9405a34797a433ec01f922a5be2def5
Erik Peteri <peteri.erik@gmail.com>
2024-12-03 20:28:24 +0200

    simplify folder structure and fix seed.py

--- twitter/management/commands/seed.py
+++ twitter/management/commands/seed.py
@@ -1,19 +1,19 @@
 from django.core.management.base import BaseCommand
-from django.contrib.auth.models import User
+from ...models import User
 
 class Command(BaseCommand):
     help = 'Seeds the database with two users'
 
     def handle(self, *args, **kwargs):
         if not User.objects.filter(username='user1').exists():
-            user1 = User.objects.create_user(
+            user1 = User.objects.create(
                 username='user1',
                 password='password1',
             )
             self.stdout.write(self.style.SUCCESS('Successfully created user1'))
 
         if not User.objects.filter(username='user2').exists():
-            user2 = User.objects.create_user(
+            user2 = User.objects.create(
                 username='user2',
                 password='password2',
             )
